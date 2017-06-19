41dbac1c7bb7a89e56c9435543b0ba42fda3d788
Mohamed AbdelraZek <muhamedrabi3@gmail.com>
2017-06-19 07:54:54 +0200

    final stable version

---
+++ app/src/main/java/com/salma/lotra/LocationActivity.java
@@ -0,0 +1,313 @@
+package com.salma.lotra;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class LocationActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks, android.location.LocationListener {
+
+    private static final int REQUEST_LOCATION = 121;
+    private FirebaseAuth mFirebaseAuth;
+    private ImageView mLocationTxtView;
+    private GoogleApiClient mGoogleApiClient;
+    private double mLatitude;
+    private double mLongitude;
+    private DatabaseReference mDatabase;
+    private AlertDialog dialog;
+    private String mBusNumber;
+    private LocationManager locationManager;
+    private String provider;
+    DriverModel mDriverModel;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .build();
+
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        provider = locationManager.getBestProvider(new Criteria(), true);
+        mLocationTxtView = (ImageView) findViewById(R.id.location_img_view);
+        mDriverModel = new DriverModel();
+
+
+        mLocationTxtView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                updateInfo();
+
+            }
+        });
+
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+
+        if (getBusNumber().equalsIgnoreCase("")) {
+            dialog = new AlertDialog.Builder(this)
+                    .create();
+            LayoutInflater layoutInflater = LayoutInflater.from(this);
+            dialog.setView(layoutInflater.inflate(R.layout.message_dialog, null));
+            dialog.show();
+
+            Button button = (Button) dialog.findViewById(R.id.ok);
+            final EditText editText = (EditText) dialog.findViewById(R.id.bus_number_edit_txt);
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mBusNumber = editText.getText().toString();
+
+                    if (mBusNumber.length() < 5) {
+                        Toast.makeText(LocationActivity.this, "Please, enter a valid Bus Number !", Toast.LENGTH_SHORT).show();
+                    } else {
+                        setBusNumber(mBusNumber);
+                        Toast.makeText(LocationActivity.this, "Bus Number saved !", Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+                }
+            });
+        }
+
+
+    }
+
+    private void updateInfo() {
+
+        mDriverModel.Latitude = mLatitude;
+        mDriverModel.Longitude = mLongitude;
+        mDriverModel.BusNumber = getBusNumber();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("DriverInfo").child(mDriverModel.DriverName).setValue(mDriverModel);
+    }
+
+    public void setBusNumber(String busNumber) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("BusNumber", busNumber);
+        editor.apply();
+    }
+
+    public String getBusNumber() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String name = preferences.getString("BusNumber", "");
+        return name;
+    }
+
+    public void deleteSharedPref() {
+        PreferenceManager.getDefaultSharedPreferences(this).edit().clear().commit();
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+
+        mDriverModel.DriverName = mFirebaseAuth.getCurrentUser().getDisplayName();
+        mDriverModel.Email = mFirebaseAuth.getCurrentUser().getEmail();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (checkLocationPermission()) {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                locationManager.requestLocationUpdates(provider, 401, 1, this);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+
+            case R.id.sign_out_menu:
+
+                cleanUp();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    private void cleanUp() {
+        mGoogleApiClient.disconnect();
+        ProgressDialog pd = new ProgressDialog(LocationActivity.this);
+        pd.setMessage("loading");
+        pd.show();
+        AuthUI.getInstance()
+                .signOut(this)
+
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+                            finish();
+                        }
+                    }
+                });
+        deleteSharedPref();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+        checkLocationPermission();
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                new AlertDialog.Builder(this)
+                        .setTitle("Permission is required !")
+                        .setMessage("The app will shut down if the permission denied! ")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(LocationActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        REQUEST_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+                      //  locationManager.requestLocationUpdates(provider, 100, 1, this);
+                    }
+
+
+                } else {
+
+                }
+                return;
+            }
+
+        }
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Toast.makeText(this, "onLocationChanged" + location.getLatitude(), Toast.LENGTH_SHORT).show();
+
+        mLatitude = location.getLatitude();
+        mLongitude = location.getLongitude();
+        updateInfo();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
---
+++ app/src/main/java/com/salma/lotra/SignInActivity.java
@@ -0,0 +1,87 @@
+package com.salma.lotra;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ResultCodes;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+
+public class SignInActivity extends AppCompatActivity {
+    private static final int FLAG_FOR_SIGN_IN = 1;
+    private FirebaseAuth mFireBaseAuth;
+    private FirebaseAuth.AuthStateListener zAuthStateListener;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        if (!isNetworkAvailable()) {
+            Toast.makeText(this, "Check the internet connection and try again !", Toast.LENGTH_SHORT).show();
+            // finish();
+        }
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(this, LocationActivity.class));
+            finish();
+            return;
+        }
+
+        mFireBaseAuth = FirebaseAuth.getInstance();
+        zAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                startActivityForResult(
+                        AuthUI.getInstance()
+                                .createSignInIntentBuilder().setIsSmartLockEnabled(false)
+                                .setAvailableProviders(
+                                        Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                                new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build())).setLogo(R.drawable.ic_launcher)
+                                .build(),
+                        FLAG_FOR_SIGN_IN);
+            }
+        };
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == FLAG_FOR_SIGN_IN) {
+            if (resultCode == ResultCodes.OK) {
+                startActivity(new Intent(this, LocationActivity.class));
+                finish();
+            }
+        }
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFireBaseAuth.addAuthStateListener(zAuthStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mFireBaseAuth.removeAuthStateListener(zAuthStateListener);
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+}
